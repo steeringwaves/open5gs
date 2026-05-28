38558ff6604666cf4714c3f790675af3457247f1
1800alex <1800alex@gmail.com>
2026-05-28 11:28:42 -0400

    revert

--- lib/dbi/ogs-dbi-core.c
+++ lib/dbi/ogs-dbi-core.c
@@ -3,18 +3,7 @@
  *
  * This file is part of Open5GS.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 #include "ogs-dbi.h"
@@ -43,6 +32,7 @@ void ogs_dbi_change_event_free(ogs_dbi_change_event_t *e)
 {
     if (!e)
         return;
-    ogs_free(e->imsi_bcd);
+    if (e->imsi_bcd)
+        ogs_free(e->imsi_bcd);
     ogs_free(e);
 }
